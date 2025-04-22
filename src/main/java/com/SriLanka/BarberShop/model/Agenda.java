07989234efe8e4933dbc84416ce05dae18f580ca
JOAO VICTOR AMARO ALVES <joao.vaalves2@senacsp.edu.br>
2025-04-22 18:42:40 -0300

    Envio de emais adicionado

--- src/main/java/com/SriLanka/BarberShop/model/Agenda.java
+++ src/main/java/com/SriLanka/BarberShop/model/Agenda.java
@@ -3,9 +3,7 @@ package com.SriLanka.BarberShop.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
 @Setter
--- src/main/java/com/SriLanka/BarberShop/services/AgendamentoService.java
+++ src/main/java/com/SriLanka/BarberShop/services/AgendamentoService.java
@@ -27,6 +27,9 @@ public class AgendamentoService {
     @Autowired
     private EstabelecimentoRepository estabelecimentoRepository;
 
+    @Autowired
+    private  EmailService emailService;
+
     public Agenda agendar(AgendamentoRequest request){
 
         Usuario cliente = usuarioRepository.findById(request.getClienteId()).orElseThrow();
@@ -40,7 +43,37 @@ public class AgendamentoService {
         agenda.setDataHora(request.getDataHora());
         agenda.setObservacoes(request.getObservacoes());
 
+        enviarEmailBarbeiro(agenda);
+        enviarEmailCliente(agenda);
+    
+
         return agendaRepository.save(agenda);
+    }
+
+    public String enviarEmailBarbeiro(Agenda agenda){
+        String emailBarbeiro = String.format(
+            "Novo corte agendado!\n\nCliente: %s\nData: %s",
+            agenda.getCliente().getNome(),
+            agenda.getDataHora());
+
+            emailService.enviarEmail(agenda.getServico().getBarbeiro().getEmail(), "Novo corte agendado", emailBarbeiro);
+
+            return emailBarbeiro;
+    }
+
+    public String enviarEmailCliente(Agenda agenda){
+        String endereco = formatarEndereco(agenda.getEstabelecimento());
+
+        String emailCliente = String.format(
+            "Olá %s, seu corte foi marcado!\n\nLocal: %s\nBarbeiro: %s\nData e Hora : %s",
+            agenda.getCliente().getNome(),
+            endereco,
+            agenda.getServico().getBarbeiro().getNome(),
+            agenda.getDataHora());
+    
+        emailService.enviarEmail(agenda.getCliente().getEmail(), "Confirmação de corte", emailCliente);
+
+        return emailCliente;
     }
 
     public List<Agenda> listarTodos(){
@@ -49,6 +82,13 @@ public class AgendamentoService {
 
     public Optional<Agenda> buscarPorId(Long id) {
         return agendaRepository.findById(id);
+    }
+
+    public String formatarEndereco(Estabelecimento barbearia){
+        return String.format("%s, %s - %s, %s - %s, %s - %s",
+        barbearia.getRua(),barbearia.getNumero(),
+        barbearia.getComplemento() != null ? barbearia.getComplemento() : " ",
+        barbearia.getBairro(), barbearia.getCidade(), barbearia.getEstado(), barbearia.getCep());
     }
 
 }
